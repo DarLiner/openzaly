99ce6ebf7fc2bbb32fe9c8702bbddd9397e418e8
SAM2O2O <an.guoyue254@gmail.com>
2018-03-21 10:44:47 +0800

    增加初始化站点扩展

--- openzaly-common/src/main/java/com/akaxin/common/utils/StringHelper.java
+++ openzaly-common/src/main/java/com/akaxin/common/utils/StringHelper.java
@@ -1,5 +1,7 @@
 package com.akaxin.common.utils;
 
+import java.util.Random;
+
 import org.slf4j.helpers.FormattingTuple;
 import org.slf4j.helpers.MessageFormatter;
 
@@ -43,5 +45,18 @@ public class StringHelper {
 	public static String format(String messagePattern, Object... objects) {
 		FormattingTuple format = MessageFormatter.arrayFormat(messagePattern, objects);
 		return format.getMessage();
+	}
+
+	private static final String STR_62_RANDOM = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	// 随机生成N位字符串（A-Z，a-z，0-9）
+	public static String generateRandomString(int length) {
+		Random random = new Random();
+		StringBuffer newRandomStr = new StringBuffer();
+		for (int i = 0; i < length; ++i) {
+			int number = random.nextInt(STR_62_RANDOM.length());
+			newRandomStr.append(STR_62_RANDOM.charAt(number));
+		}
+		return newRandomStr.toString();
 	}
 }
--- openzaly-storage/src/main/java/com/akaxin/site/storage/sqlite/manager/SQLiteJDBCManager.java
+++ openzaly-storage/src/main/java/com/akaxin/site/storage/sqlite/manager/SQLiteJDBCManager.java
@@ -26,6 +26,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.akaxin.common.utils.StringHelper;
 import com.akaxin.proto.core.ConfigProto;
 import com.akaxin.proto.core.PluginProto;
 import com.akaxin.proto.core.UicProto;
@@ -72,9 +73,9 @@ public class SQLiteJDBCManager {
 		checkDatabaseTable();
 		checkDatabaseIndex();
 		initSiteConfig(config.getConfigMap());
-		addSitePlugin(1, PluginArgs.SITE_ADMIN_NAME, config.getSiteServer(), config.getAdminApi(),
+		addSitePlugin(1, PluginArgs.SITE_ADMIN_NAME, config.getAdminApi(), config.getSiteServer(),
 				config.getAdminIcon());
-		addSitePlugin(2, PluginArgs.FRIEND_SQUARE_NAME, PluginArgs.FRIEND_SQUARE_ADDRESS, PluginArgs.FRIEND_SQUARE_API,
+		addSitePlugin(2, PluginArgs.FRIEND_SQUARE_NAME, PluginArgs.FRIEND_SQUARE_API, PluginArgs.FRIEND_SQUARE_ADDRESS,
 				config.getParam(PluginArgs.FRIEND_SQUARE, String.class));
 		initAdminUic(config.getAdminUic());
 	}
@@ -191,22 +192,38 @@ public class SQLiteJDBCManager {
 		return result;
 	}
 
-	private static void addSitePlugin(int id, String siteName, String urlPage, String urlApi, String siteIcon) {
+	private static void addSitePlugin(int id, String siteName, String urlPage, String apiUrl, String siteIcon) {
 		boolean result = false;
-		String updateSql = "UPDATE site_plugin_manager SET name=?,url_page=?,url_api=? WHERE id=?;";
-		String insertSql = "INSERT INTO site_plugin_manager(id,name,icon,url_page,url_api,status) VALUES(?,?,?,?,?,?);";
+		String updateSql = "UPDATE site_plugin_manager SET "//
+				+ "name=?,"//
+				+ "url_page=?,"//
+				+ "api_url=?"//
+				+ " WHERE id=?;";//
+		String insertSql = "INSERT INTO site_plugin_manager("//
+				+ "id,"//
+				+ "name,"//
+				+ "icon,"//
+				+ "url_page,"//
+				+ "api_url,"//
+				+ "auth_key,"//
+				+ "allowed_ip,"//
+				+ "position,"//
+				+ "sort,"//
+				+ "display_mode,"//
+				+ "permission_status,"//
+				+ "add_time) VALUES(?,?,?,?,?,?,?,?,?,?,?,?);";
 		try {
 			PreparedStatement pst = sqlitConnection.prepareStatement(updateSql);
 			pst.setString(1, siteName);
 			pst.setString(2, urlPage);
-			pst.setString(3, urlApi);
+			pst.setString(3, apiUrl);
 			pst.setInt(4, id);
 
 			result = (pst.executeUpdate() > 0);
-			logger.info("update site management result={} SQL={} name={} url_page={} url_api={}", result, updateSql,
-					siteName, urlPage, urlApi);
+			logger.info("update site plugin result={} SQL={} name={} url_page={} url_api={}", result, updateSql,
+					siteName, urlPage, apiUrl);
 		} catch (SQLException e) {
-			logger.error("update site management error", e);
+			logger.error("update site plugin error", e);
 		}
 
 		try {
@@ -216,19 +233,29 @@ public class SQLiteJDBCManager {
 				pst.setString(2, siteName);
 				pst.setString(3, siteIcon);
 				pst.setString(4, urlPage);
-				pst.setString(5, urlApi);
-
+				pst.setString(5, apiUrl);
+				pst.setString(6, StringHelper.generateRandomString(64));// authkey
 				if (id == 1) {// 默认为后台管理
-					pst.setInt(6, PluginProto.PluginPosition.HOME_PAGE_VALUE);
+					pst.setString(7, "127.0.0.1");// allowed_ip
+					pst.setInt(8, PluginProto.PluginPosition.HOME_PAGE_VALUE);// position
+					pst.setInt(9, 0);// sort
+					pst.setInt(10, PluginProto.PluginDisplayMode.NEW_PAGE_VALUE); // display_mode
+					pst.setInt(11, PluginProto.PermissionStatus.DISABLED_VALUE); // permission_status
+					pst.setLong(12, System.currentTimeMillis()); // add_time
 				} else {
-					pst.setInt(6, PluginProto.PluginPosition.MSG_PAGE_VALUE);
+					pst.setString(7, "127.0.0.1");// allowed_ip
+					pst.setInt(8, PluginProto.PluginPosition.MSG_PAGE_VALUE);// position
+					pst.setInt(9, 1);// sort
+					pst.setInt(10, PluginProto.PluginDisplayMode.NEW_PAGE_VALUE); // display_mode
+					pst.setInt(11, PluginProto.PermissionStatus.DISABLED_VALUE); // permission_status
+					pst.setLong(12, System.currentTimeMillis()); // add_time
 				}
 				result = (pst.executeUpdate() > 0);
-				logger.info("insert site management result={} SQL={} name={} url_page={} url_api={}", result, insertSql,
-						siteName, urlPage, urlApi);
+				logger.info("insert site plugin result={} SQL={} name={} url_page={} url_api={}", result, insertSql,
+						siteName, urlPage, apiUrl);
 			}
 		} catch (SQLException e) {
-			logger.error("insert site management error", e);
+			logger.error("insert site plugin error", e);
 		}
 	}
 
