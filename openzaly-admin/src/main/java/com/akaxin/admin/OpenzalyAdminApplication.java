b3d220dd5724a37f48bbe2c684b9894a01be152b
SAM2O2O <an.guoyue254@gmail.com>
2018-04-16 12:42:21 +0800

    修改openzaly-admin 中pom依赖springbooter的方式

--- openzaly-admin/src/main/java/com/akaxin/admin/OpenzalyAdminApplication.java
+++ openzaly-admin/src/main/java/com/akaxin/admin/OpenzalyAdminApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenzalyAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenzalyAdminApplication.class, args);
+    		SpringApplication.run(OpenzalyAdminApplication.class, args);
     }
 }
