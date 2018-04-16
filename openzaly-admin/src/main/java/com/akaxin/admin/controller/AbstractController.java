0b55f5375d2d1d7024f026f78e20443abc4714f9
SAM2O2O <an.guoyue254@gmail.com>
2018-04-16 21:13:46 +0800

    add abstract contorller

---
+++ openzaly-admin/src/main/java/com/akaxin/admin/controller/AbstractController.java
@@ -0,0 +1,11 @@
+package com.akaxin.admin.controller;
+
+public class AbstractController {
+
+	protected final String SUCCESS = "success";
+
+	protected final String ERROR = "error";
+
+	protected final String NO_PERMISSION = "no-permission";
+
+}
