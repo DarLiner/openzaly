4fc1b6adb16e3c7dc1220ceb12dcfb37895aab1c
mino0885 <ljh980806>
2018-04-17 20:36:34 +0800

    Plugin controller and  html done

--- openzaly-admin/src/main/java/com/akaxin/admin/controller/PluginManageController.java
+++ openzaly-admin/src/main/java/com/akaxin/admin/controller/PluginManageController.java
@@ -7,10 +7,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,178 +27,206 @@ import com.akaxin.site.storage.bean.PluginBean;
 @Controller
 @RequestMapping("plugin")
 public class PluginManageController extends AbstractController {
-	private static final Logger logger = LoggerFactory.getLogger(UserManageController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserManageController.class);
 
-	@Autowired
-	@Qualifier("pluginManageService")
-	private IPluginService pluginService;
+    @Autowired
+    private IPluginService pluginService;
 
-	@RequestMapping("/indexPage")
-	public ModelAndView toPluginIndex() {
-		ModelAndView modelAndView = new ModelAndView("plugin/index");
-		return modelAndView;
-	}
+    @RequestMapping("/indexPage")
+    public ModelAndView toPluginIndex() {
+        ModelAndView modelAndView = new ModelAndView("plugin/index");
+        return modelAndView;
+    }
 
-	@RequestMapping("/addPage")
-	public ModelAndView toPluginAdd() {
-		ModelAndView modelAndView = new ModelAndView("plugin/add");
-		return modelAndView;
-	}
+    @RequestMapping("/addPage")
+    public String toPluginAdd() {
+        return "plugin/add";
+    }
 
-	@RequestMapping("/listPage")
-	public ModelAndView toPluginList() {
-		ModelAndView modelAndView = new ModelAndView("plugin/list");
-		return modelAndView;
-	}
+    @RequestMapping("/listPage")
+    public String toPluginList() {
+        return "plugin/list";
+    }
 
-	// 增加新扩展
-	@RequestMapping(method = RequestMethod.POST, value = "/addPlugin")
-	@ResponseBody
-	public String addPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
-		try {
-			PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
+    @RequestMapping("/editPage")
+    public ModelAndView toEditPage(@RequestBody byte[] bodyParam) {
+        ModelAndView modelAndView = new ModelAndView("plugin/update");
+        Map<String, Object> model = modelAndView.getModel();
+        PluginProto.ProxyPluginPackage pluginPackage = null;
+        try {
+            pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
 
-			String siteUserId = getRequestSiteUserId(pluginPackage);
-			if (isManager(siteUserId)) {
-				Map<String, String> pluginData = getRequestDataMap(pluginPackage);
-				logger.info("siteUserId={} add new plugin={}", siteUserId, pluginData);
+            String siteUserId = getRequestSiteUserId(pluginPackage);
+            if (isManager(siteUserId)) {
+                //解析Plugin_id
+                String data = pluginPackage.getData();
+                String[] split = data.split(":\"");
+                String res = split[1].replaceAll("\"}", "");
+                PluginBean plugin = pluginService.getPlugin(Integer.valueOf(res));
+                model.put("name", plugin.getName());
+                model.put("url_page", plugin.getUrlPage());
+                model.put("api_url", plugin.getApiUrl());
+                model.put("plugin_icon", plugin.getIcon());
+                model.put("order", plugin.getSort());
+                model.put("allow_ip", plugin.getAllowedIp());
+                model.put("position", plugin.getPosition());
+                model.put("per_status", plugin.getPermissionStatus());
+                model.put("id", plugin.getId());
+            }
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        return modelAndView;
+    }
 
-				PluginBean bean = new PluginBean();
-				bean.setName(pluginData.get("name"));
-				bean.setIcon(pluginData.get("plugin_icon"));
-				bean.setUrlPage(pluginData.get("url_page"));
-				bean.setApiUrl(pluginData.get("api_url"));
-				bean.setAllowedIp(pluginData.get("allow_ip"));
-				bean.setPosition(Integer.valueOf(pluginData.get("position")));
-				bean.setSort(Integer.valueOf(pluginData.get("order")));
-				bean.setDisplayMode(PluginProto.PluginDisplayMode.NEW_PAGE_VALUE);
-				bean.setPermissionStatus(Integer.valueOf(pluginData.get("per_status")));
-				bean.setAddTime(System.currentTimeMillis());
-				bean.setAuthKey(StringHelper.generateRandomString(16));// 随机生成
+    // 增加新扩展
+    @RequestMapping(method = RequestMethod.POST, value = "/addPlugin")
+    @ResponseBody
+    public String addPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
+        try {
+            PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
 
-				logger.info("siteUserId={} add new plugin bean={}", siteUserId, bean);
-				if (pluginService.addNewPlugin(bean)) {
-					return SUCCESS;
-				}
+            String siteUserId = getRequestSiteUserId(pluginPackage);
+            if (isManager(siteUserId)) {
+                Map<String, String> pluginData = getRequestDataMap(pluginPackage);
+                logger.info("siteUserId={} add new plugin={}", siteUserId, pluginData);
 
-			} else {
-				return NO_PERMISSION;
-			}
-		} catch (Exception e) {
-			logger.error("add new plugin controller error", e);
-		}
-		return ERROR;
-	}
+                PluginBean bean = new PluginBean();
+                bean.setName(pluginData.get("name"));
+                bean.setIcon(pluginData.get("plugin_icon"));
+                bean.setUrlPage(pluginData.get("url_page"));
+                bean.setApiUrl(pluginData.get("api_url"));
+                bean.setAllowedIp(pluginData.get("allow_ip"));
+                bean.setPosition(Integer.valueOf(pluginData.get("position")));
+                bean.setSort(Integer.valueOf(pluginData.get("order")));
+                bean.setDisplayMode(PluginProto.PluginDisplayMode.NEW_PAGE_VALUE);
+                bean.setPermissionStatus(Integer.valueOf(pluginData.get("per_status")));
+                bean.setAddTime(System.currentTimeMillis());
+                bean.setAuthKey(StringHelper.generateRandomString(16));// 随机生成
 
-	// 获取扩展列表
-	@RequestMapping(method = RequestMethod.POST, value = "/getPlugin")
-	@ResponseBody
-	public Map<String, Object> getPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
+                logger.info("siteUserId={} add new plugin bean={}", siteUserId, bean);
+                if (pluginService.addNewPlugin(bean)) {
+                    return SUCCESS;
+                }
 
-		return null;
-	}
+            } else {
+                return NO_PERMISSION;
+            }
+        } catch (Exception e) {
+            logger.error("add new plugin controller error", e);
+        }
+        return ERROR;
+    }
 
-	// 获取扩展列表
-	@RequestMapping(method = RequestMethod.POST, value = "/pluginList")
-	@ResponseBody
-	public Map<String, Object> getPluginList(HttpServletRequest request, @RequestBody byte[] bodyParam) {
-		Map<String, Object> result = new HashMap<String, Object>();
-		boolean nodata = true;// 是还有更多数据
-		try {
-			PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
-			String siteUserId = getRequestSiteUserId(pluginPackage);
+    // 获取扩展列表
+    @RequestMapping(method = RequestMethod.POST, value = "/getPlugin")
+    @ResponseBody
+    public Map<String, Object> getPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
 
-			if (isManager(siteUserId)) {
-				Map<String, String> dataMap = getRequestDataMap(pluginPackage);
-				int pageNum = Integer.valueOf(dataMap.get("page"));
-				logger.info("siteUserId={} get plugin list pageNum={} pageSize={}", siteUserId, pageNum, PAGE_SIZE);
+        return null;
+    }
 
-				List<PluginBean> pluginList = pluginService.getPluginList(pageNum, PAGE_SIZE);
-				List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
-				if (pluginList != null) {
-					if (PAGE_SIZE == pluginList.size()) {
-						nodata = false;
-					}
-					for (PluginBean bean : pluginList) {
-						Map<String, Object> pluginMap = new HashMap<String, Object>();
-						pluginMap.put("plugin_id", bean.getId());
-						pluginMap.put("name", bean.getName());
-						pluginMap.put("plugin_icon", bean.getIcon());
-						pluginMap.put("url_page", bean.getUrlPage());
-						pluginMap.put("api_url", bean.getApiUrl());
-						pluginMap.put("position", bean.getPosition());
-						pluginMap.put("order", bean.getSort());
-						pluginMap.put("per_status", bean.getPermissionStatus());
-						pluginMap.put("allow_ip", bean.getAllowedIp());
-						// add to list
-						data.add(pluginMap);
-					}
-				}
+    // 获取扩展列表
+    @RequestMapping(method = RequestMethod.POST, value = "/pluginList")
+    @ResponseBody
+    public Map<String, Object> getPluginList(HttpServletRequest request, @RequestBody byte[] bodyParam) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        boolean nodata = true;// 是还有更多数据
+        try {
+            PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
+            String siteUserId = getRequestSiteUserId(pluginPackage);
 
-				result.put("pluginData", data);
-			}
+            if (isManager(siteUserId)) {
+                Map<String, String> dataMap = getRequestDataMap(pluginPackage);
+                int pageNum = Integer.valueOf(dataMap.get("page"));
+                logger.info("get plugin list ");
+                List<PluginBean> pluginList = pluginService.getPluginList(pageNum, PAGE_SIZE);
+                List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+                if (pluginList != null) {
+                    if (PAGE_SIZE == pluginList.size()) {
+                        nodata = false;
+                    }
+                    for (PluginBean bean : pluginList) {
+                        Map<String, Object> pluginMap = new HashMap<String, Object>();
+                        pluginMap.put("plugin_id", bean.getId());
+                        pluginMap.put("name", bean.getName());
+                        pluginMap.put("plugin_icon", bean.getIcon());
+                        pluginMap.put("url_page", bean.getUrlPage());
+                        pluginMap.put("api_url", bean.getApiUrl());
+                        pluginMap.put("position", bean.getPosition());
+                        pluginMap.put("order", bean.getSort());
+                        pluginMap.put("per_status", bean.getPermissionStatus());
+                        pluginMap.put("allow_ip", bean.getAllowedIp());
+                        // add to list
+                        data.add(pluginMap);
+                    }
+                }
 
-		} catch (Exception e) {
-			logger.error("get plugin list error", e);
-		}
-		result.put("loading", nodata);
-		return result;
-	}
+                result.put("pluginData", pluginList);
+            }
 
-	// 编辑扩展
-	@RequestMapping(method = RequestMethod.POST, value = "/updatePlugin")
-	@ResponseBody
-	public String editPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
-		try {
-			PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
-			String siteUserId = getRequestSiteUserId(pluginPackage);
-			if (isManager(siteUserId)) {
-				Map<String, String> pluginData = getRequestDataMap(pluginPackage);
-				logger.info("siteUserId={} update plugin={}", siteUserId, pluginData);
+        } catch (Exception e) {
+            logger.error("get plugin list error", e);
+        }
+        result.put("loading", nodata);
+        return result;
+    }
 
-				PluginBean bean = new PluginBean();
-				bean.setId(Integer.valueOf(pluginData.get("plugin_id")));
-				bean.setName(pluginData.get("name"));
-				bean.setIcon(pluginData.get("plugin_icon"));
-				bean.setUrlPage(pluginData.get("url_page"));
-				bean.setApiUrl(pluginData.get("api_url"));
-				bean.setPosition(Integer.valueOf(pluginData.get("position")));
-				bean.setSort(Integer.valueOf(pluginData.get("order")));
-				bean.setPermissionStatus(Integer.valueOf(pluginData.get("per_status")));
-				bean.setAllowedIp(pluginData.get("allow_ip"));
-				logger.info("siteUserId={} update plugin bean={}", siteUserId, bean);
+    // 编辑扩展
+    @RequestMapping(method = RequestMethod.POST, value = "/editPlugin")
+    @ResponseBody
 
-				if (pluginService.updatePlugin(bean)) {
-					return SUCCESS;
-				}
+    public String editPlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
+        try {
+            PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
+            String siteUserId = getRequestSiteUserId(pluginPackage);
+            if (isManager(siteUserId)) {
+                Map<String, String> pluginData = getRequestDataMap(pluginPackage);
+                logger.info("siteUserId={} update plugin={}", siteUserId, pluginData);
 
-			}
-		} catch (Exception e) {
-			logger.error("edit plugin error", e);
-		}
-		return ERROR;
-	}
+                PluginBean bean = new PluginBean();
+                bean.setId(Integer.valueOf(pluginData.get("plugin_id")));
+                bean.setName(pluginData.get("name"));
+                bean.setIcon(pluginData.get("plugin_icon"));
+                bean.setUrlPage(pluginData.get("url_page"));
+                bean.setApiUrl(pluginData.get("api_url"));
+                bean.setPosition(Integer.valueOf(pluginData.get("position")));
+                bean.setSort(Integer.valueOf(pluginData.get("order")));
+                bean.setPermissionStatus(Integer.valueOf(pluginData.get("per_status")));
+                bean.setAllowedIp(pluginData.get("allow_ip"));
+                logger.info("siteUserId={} update plugin bean={}", siteUserId, bean);
 
-	@RequestMapping(method = RequestMethod.POST, value = "/delPlugin")
-	@ResponseBody
-	public String deletePlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
-		try {
-			PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
-			String siteUserId = getRequestSiteUserId(pluginPackage);
-			if (isManager(siteUserId)) {
-				Map<String, String> dataMap = getRequestDataMap(pluginPackage);
-				int pluginId = Integer.valueOf(dataMap.get("plugin_id"));
+                if (pluginService.updatePlugin(bean)) {
+                    return SUCCESS;
+                }
 
-				if (pluginService.deletePlugin(pluginId)) {
-					return SUCCESS;
-				}
-			} else {
-				return NO_PERMISSION;
-			}
-		} catch (Exception e) {
-			logger.error("edit plugin error", e);
-		}
-		return ERROR;
-	}
+            }
+        } catch (Exception e) {
+            logger.error("edit plugin error", e);
+        }
+        return ERROR;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/delPlugin")
+    @ResponseBody
+    public String deletePlugin(HttpServletRequest request, @RequestBody byte[] bodyParam) {
+        try {
+            PluginProto.ProxyPluginPackage pluginPackage = PluginProto.ProxyPluginPackage.parseFrom(bodyParam);
+            String siteUserId = getRequestSiteUserId(pluginPackage);
+            if (isManager(siteUserId)) {
+                Map<String, String> dataMap = getRequestDataMap(pluginPackage);
+                int pluginId = Integer.valueOf(dataMap.get("plugin_id"));
+
+                if (pluginService.deletePlugin(pluginId)) {
+                    return SUCCESS;
+                }
+            } else {
+                return NO_PERMISSION;
+            }
+        } catch (Exception e) {
+            logger.error("edit plugin error", e);
+        }
+        return ERROR;
+    }
 
 }
