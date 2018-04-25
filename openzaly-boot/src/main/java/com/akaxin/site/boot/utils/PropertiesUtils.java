81a068ffbed3702232371602d63021dd2a6bd19c
SAM2O2O <an.guoyue254@gmail.com>
2018-04-25 16:34:19 +0800

    还原默认系统环境

--- openzaly-boot/src/main/java/com/akaxin/site/boot/utils/PropertiesUtils.java
+++ openzaly-boot/src/main/java/com/akaxin/site/boot/utils/PropertiesUtils.java
@@ -29,8 +29,8 @@ public class PropertiesUtils {
 
 	public static Properties getDefaultProperties() {
 		Properties properties = new Properties();
-		properties.put("site.project.env", "DEBUG");
-//		properties.put("site.project.env", "ONLINE");
+//		properties.put("site.project.env", "DEBUG");
+		properties.put("site.project.env", "ONLINE");
 		properties.put("site.version", "0.3.2");
 		properties.put("site.address", "0.0.0.0");
 		properties.put("site.port", "2021");
