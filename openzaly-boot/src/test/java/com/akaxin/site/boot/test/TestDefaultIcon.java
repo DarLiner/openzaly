c4184c92df6670688780e487e36c5f692b63f74b
SAM2O2O <an.guoyue254@gmail.com>
2018-05-08 10:54:06 +0800

    update default adminmanager icon

--- openzaly-boot/src/test/java/com/akaxin/site/boot/test/TestDefaultIcon.java
+++ openzaly-boot/src/test/java/com/akaxin/site/boot/test/TestDefaultIcon.java
@@ -10,7 +10,7 @@ import com.akaxin.site.business.utils.FileServerUtils;
 public class TestDefaultIcon {
 	public static void main(String args[]) {
 		// String fileUrl = "site-admin-icon.jpg";
-		String fileUrl = "friend-square-icon.jpg";
+		String fileUrl = "管理.png";
 		byte[] fileBytes = FileServerUtils.fileToBinary(fileUrl);
 
 		String str = Base64.getEncoder().encodeToString(fileBytes);
