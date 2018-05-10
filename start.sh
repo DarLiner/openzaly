84331f7be3fd4b7bc09daf579fba63ff7b1243d5
SAM2O2O <an.guoyue254@gmail.com>
2018-05-10 14:07:16 +0800

    add echo akaxin logo and desc for restart.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PORT=$1
+PORT2=$2
+IS_RESTART=$3
+
+if [ -z $IS_RESTART ];then
 #### echo akaxin logo and desc
 echo "
     _      _  __     _     __  __  ___   _   _ 
@@ -14,9 +19,8 @@ java-version : JDK 1.8+
 maven-version : 3.0+
 
 "
+fi
 
-PORT=$1
-PORT2=$2
 
 ##set tcp port
 if [ -n $PORT ]; then
@@ -29,15 +33,16 @@ if [ -n $PORT2 ]; then
 fi
 
 #### echo server is starting
-echo "【OK】openzaly-server is starting 【tcp-port:"$PORT" http-port:"$PORT2"】"
+echo "[OK] openzaly-server is starting  [tcp-port:"$PORT" http-port:"$PORT2"]"
 
 JAVA_JAR="openzaly-server"
 PID=$(ps -ef|grep $JAVA_JAR|grep $PORT |head -1| awk '{printf $2}')
 
 ###if server is running, exit and echo error
 if [ $PID > 0 ]; then
-    echo "【ERROR】openzaly-server is running PID:"$PID
-    echo "【ERROR】openzaly-server start failure"
+    echo "[ERROR] openzaly-server is running PID:"$PID
+    echo "[ERROR] openzaly-server start failure"
+    echo ""
     exit
 fi
 
@@ -46,11 +51,12 @@ java -Dsite.port=$PORT -Dhttp.port=$PORT2 -jar openzaly-server.jar >>stdout.log 
 PID=$(ps -ef|grep $JAVA_JAR|grep $PORT |head -1| awk '{printf $2}')
 
 if [ $? -eq 0 ]; then
-    echo "【OK】openzaly-server tcp-port:"$PORT",http-port:$PORT2,PID:$PID"
-    echo "【OK】openzaly-server is started successfully 【PID:"$PID"】"
+    echo "[OK] openzaly-server tcp-port:"$PORT",http-port:$PORT2,PID:$PID"
+    echo "[OK] openzaly-server is started successfully  [PID:"$PID"]"
 else
-    echo "【ERROR】openzaly-server is started failed"
+    echo "[ERROR] openzaly-server is started failed"
     echo "exit..."
+    echo ""
     exit
 fi
 
--- stop.sh
+++ stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+PORT=$1
+IS_RESTART=$2
+JAVA_JAR="openzaly-server"
+
+## IS_RESTART not exist ,echo info
+if [ -z $IS_RESTART ]; then
 #### echo akaxin logo and desc
 echo "
+
     _      _  __     _     __  __  ___   _   _ 
    / \    | |/ /    / \    \ \/ / |_ _| | \ | |
   / _ \   | ' /    / _ \    \  /   | |  |  \| |
@@ -12,23 +19,24 @@ Akaxin is an open source and free proprietary IM software，you can build privat
 openzaly-version : 0.5.4
 
 "
+fi
 
-PORT=$1
-JAVA_JAR="openzaly-server"
 
 ##set tcp port
 if [ -n $PORT ]; then
 	PORT=2021
 fi
 	
-echo "【OK】openzaly-server is closing 【PORT="$PORT"】"
+echo "[OK] openzaly-server is closing [PORT:"$PORT"]"
 
 PID=$(ps -ef|grep $JAVA_JAR|grep $PORT |head -1| awk '{printf $2}')
 
-if [ $? -eq 0 ]; then
-    echo "【OK】openzaly-server is running on PID="$PID
+#if [ $? -eq 0 ]; then
+if [ $PID ]; then
+    echo "[OK] openzaly-server is running on PID:"$PID
 else
-    echo "【ERROR】openzaly-server stop failure, as it's not existed."
+    echo "[ERROR] openzaly-server stop failure, as it's not running."
+    echo ""
     exit
 fi
 
@@ -36,9 +44,9 @@ fi
 kill -9 ${PID}
 
 if [ $? -eq 0 ];then
-    echo "【OK】openzaly-server is stoped【PORT:"$PORT" PID:"$PID"】"
+    echo "[OK] openzaly-server is stoped[PORT:"$PORT" PID:"$PID"]"
 else
-    echo "【ERROR】openzaly-server stop failure."
+    echo "[ERROR] openzaly-server stop failure."
 fi
 
 echo ""
