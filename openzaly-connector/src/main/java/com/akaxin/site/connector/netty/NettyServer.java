0b8813b79ebce475a0717a98b1138d72bd008485
SAM2O2O <an.guoyue254@gmail.com>
2018-05-17 18:05:52 +0800

    update netty parentGroup and childGroup num

--- openzaly-connector/src/main/java/com/akaxin/site/connector/netty/NettyServer.java
+++ openzaly-connector/src/main/java/com/akaxin/site/connector/netty/NettyServer.java
@@ -63,8 +63,8 @@ public abstract class NettyServer {
 		try {
 			bootstrap = new ServerBootstrap();
 			int needThreadNum = Runtime.getRuntime().availableProcessors() + 1;
-			int parentNum = needThreadNum * 5 + 10;
-			int childNum = needThreadNum * 2 + 10;
+			int parentNum = 10;//accept from channel socket
+			int childNum = needThreadNum * 5 + 10;//give to business handler
 			// 处理服务端事件组
 			parentGroup = new NioEventLoopGroup(parentNum, new PrefixThreadFactory("bim-boss-evenloopgroup"));
 			// 处理客户端连接请求的事件组
