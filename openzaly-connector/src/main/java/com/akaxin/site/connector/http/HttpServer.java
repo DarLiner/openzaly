6e281df8d684124f8d2cdf3c540ba7b1ea75d13b
SAM2O2O <an.guoyue254@gmail.com>
2018-06-07 12:29:22 +0800

    add chunked and aggregator for netty http

--- openzaly-connector/src/main/java/com/akaxin/site/connector/http/HttpServer.java
+++ openzaly-connector/src/main/java/com/akaxin/site/connector/http/HttpServer.java
@@ -36,8 +36,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 
@@ -60,10 +62,10 @@ public abstract class HttpServer {
 			// 接受连接的可连接队列大小
 			bootstrap.option(ChannelOption.SO_BACKLOG, 120);
 			bootstrap.option(ChannelOption.SO_REUSEADDR, true);
-			//设置缓存大小
+			// 设置缓存大小
 			bootstrap.option(ChannelOption.SO_RCVBUF, 256 * 1024);
 			bootstrap.option(ChannelOption.SO_SNDBUF, 256 * 1024);// 256 KB/字节
-			
+
 			bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
 			bootstrap.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
 			/**
@@ -75,6 +77,8 @@ public abstract class HttpServer {
 				public void initChannel(SocketChannel ch) throws Exception {
 					ch.pipeline().addLast(new HttpResponseEncoder());
 					ch.pipeline().addLast(new HttpRequestDecoder());
+					ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+					ch.pipeline().addLast("streamer", new ChunkedWriteHandler());
 					ch.pipeline().addLast(new HttpServerHandler(executor));
 				}
 			});
