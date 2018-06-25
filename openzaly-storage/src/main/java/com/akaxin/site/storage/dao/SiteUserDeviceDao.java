cf65a602e24c0567b535815c1673ece41882bd69
SAM2O2O <an.guoyue254@gmail.com>
2018-06-25 18:23:03 +0800

    fix device sql bug

--- openzaly-storage/src/main/java/com/akaxin/site/storage/dao/SiteUserDeviceDao.java
+++ openzaly-storage/src/main/java/com/akaxin/site/storage/dao/SiteUserDeviceDao.java
@@ -239,13 +239,8 @@ public class SiteUserDeviceDao {
 	public UserDeviceBean queryLatestDevice(String siteUserId) throws SQLException {
 		UserDeviceBean deviceBean = new UserDeviceBean();
 		long startTime = System.currentTimeMillis();
-		// String sql = "SELECT
-		// site_user_id,device_id,user_device_pubk,device_name,max(active_time) FROM "
-		// + USER_DEVICE_TABLE + " WHERE site_user_id=? LIMIT 1;";
-
-		String sql = "SELECT a.site_user_id,a.device_id,a.user_device_pubk,a.device_name,a.active_time FROM "
-				+ USER_DEVICE_TABLE + " AS a INNER JOIN (SELECT device_id,MAX(active_time) FROM " + USER_DEVICE_TABLE
-				+ " WHERE site_user_id=? GROUP BY device_id) AS b ON a.device_id=b.device_id;";
+		String sql = "SELECT site_user_id,device_id,user_device_pubk,device_name,active_time FROM " + USER_DEVICE_TABLE
+				+ " WHERE site_user_id=? order by active_time desc LIMIT 1;";
 
 		Connection conn = null;
 		PreparedStatement pst = null;
@@ -277,7 +272,7 @@ public class SiteUserDeviceDao {
 		List<UserDeviceBean> devicesBean = new ArrayList<UserDeviceBean>();
 		long startTime = System.currentTimeMillis();
 		String sql = "SELECT site_user_id,device_id,user_device_pubk,device_name,active_time FROM " + USER_DEVICE_TABLE
-				+ " WHERE site_user_id=?;";
+				+ " WHERE site_user_id=? order by active_time desc;";
 
 		Connection conn = null;
 		PreparedStatement pst = null;
